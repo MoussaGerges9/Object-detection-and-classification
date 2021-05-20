31c2103cf0aca784a6db28d88c0e017c2bab8c23
Moussa Gerges <moussagerges9@gmail.com>
2021-05-20 09:14:47 +0200

    detect objects with higher confidence

--- main.py
+++ main.py
@@ -1,17 +1,64 @@
 import cv2
+import numpy as np
 
 cap = cv2.VideoCapture(0)
 classesFile = 'ClassesNames'
 classNames = []
-with open(classesFile,'rt') as f:
+with open(classesFile, 'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
 
-print(classNames)
-print(len(classNames))
+modelConfigurations = 'yolov3.cfg'
+modelWeights = 'yolov3.weights'
+
+confThreshold = 0.5  # the confidence
+nmsThreshold = 0.3  # less it's i'll have less boxes
+
+net = cv2.dnn.readNetFromDarknet(modelConfigurations, modelWeights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+
+def findObjects(outputs, img):
+    hT, wT, cT = img.shape
+    bbox = []
+    classIds = []
+    confs = []
+
+    for output in outputs:
+        for det in output:
+            scores = det[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > confThreshold:
+                w, h = int(det[2] * wT), int(det[3] * hT)
+                x, y = int((det[0] * wT) - w / 2), int((det[1] * hT) - h / 2)
+                bbox.append([x, y, w, h])
+                classIds.append(classId)
+                confs.append(float(confidence))
+
+    indices = cv2.dnn.NMSBoxes(bbox, confs, confThreshold,
+                               nmsThreshold)  # filter boxes to keep only the most important one
+    for i in indices:
+        i = i[0]
+        box = bbox[i]
+        x, y, w, h = box[0], box[1], box[2], box[3]
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 
 while True:
     success, img = cap.read()
+
+    blob = cv2.dnn.blobFromImage(img, 1 / 255, (320, 320), [0, 0, 0], 1, crop=False)
+    net.setInput(blob)
+
+    layerNames = net.getLayerNames()
+
+    outputNames = [layerNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    # print(outputNames)
+
+    outputs = net.forward(outputNames)
+
+    findObjects(outputs, img)
 
     cv2.imshow('Image', img)
     cv2.waitKey(1)
